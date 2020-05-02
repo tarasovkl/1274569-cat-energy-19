9d90fbd3590671c960042bd7d4ac1fa974d2a157
tarasovkl <tarasovk1991@gmail.com>
2020-05-02 17:38:38 +0300

    mobileVer

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,9 @@ gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
-    .pipe(sass())
+    .pipe(sass({
+      includePaths: require("node-normalize-scss").includePaths
+    }))
     .pipe(postcss([
       autoprefixer()
     ]))
